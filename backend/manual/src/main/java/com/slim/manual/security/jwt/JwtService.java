394369ed8f1c7a6401130afb5d247047c365dcce
NicholasRoque <nicholas.sroque@gmail.com>
2021-09-11 13:28:47 -0300

    jwt create,decode and valid

---
+++ backend/manual/src/main/java/com/slim/manual/security/jwt/JwtService.java
@@ -0,0 +1,95 @@
+package com.slim.manual.security.jwt;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+
+import com.slim.manual.domain.model.Usuario;
+
+
+@Service
+public class JwtService {
+
+    @Value("${security.jwt.expiracao}")
+    private String expiracao;
+
+    @Value("${security.jwt.chave-assinatura}")
+    private String chaveAssinatura;
+
+    /**
+     * Gera um token JWT através das informações de um usuário
+     * @param usuario
+     * @return token
+     */
+    public String gerarToken(Usuario usuario){
+        long expLong = Long.valueOf(expiracao);
+        LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(expLong);
+        Instant instant = dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant();
+        Date data = Date.from(instant);
+        
+        HashMap<String, Object> claims = new HashMap<>();
+        claims.put("codUsuario",usuario.getCodUsuario());
+        claims.put("email",usuario.getEmail());
+        claims.put("nome",usuario.getNome());
+        claims.put("role",usuario.getRole());
+
+        return Jwts
+                .builder()
+                .setSubject(usuario.getCodUsuario().toString())
+                .setExpiration(data)
+                .setClaims(claims)
+                .signWith(SignatureAlgorithm.HS512, chaveAssinatura)
+                .compact();
+    }
+
+    /**
+     * Pega as informações do usuário através de um token JWT
+     * @param token
+     * @return claims
+     * @throws ExpiredJwtException
+     */
+    private Claims obterClaims(String token) throws ExpiredJwtException{
+            return Jwts
+                    .parserBuilder()
+                    .setSigningKey(chaveAssinatura)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+    }
+
+    /**
+     * Verifica se um token JWT ainda é válido no horário atual
+     * @param token
+     * @return boolean
+     */
+    public boolean validarToken(String token){
+        try {
+            Claims claims = obterClaims(token);
+            Date data = claims.getExpiration();
+            LocalDateTime dataExp = data.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            return !LocalDateTime.now().isAfter(dataExp);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /**
+     * Retorna o código do usuário referente ao token jwt
+     * @param token
+     * @return codUsuario
+     */
+    public Integer obterCodUsuario(String token){
+        return Integer.valueOf(obterClaims(token).getSubject());
+    }
+
+}
