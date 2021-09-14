6622948ccacbf2eff976943db0cc400e16ce7220
Rafael-BD <rafael8885326@gmail.com>
2021-09-14 15:32:27 -0300

    tela login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edite <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,11 +15,12 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprenda React
         </a>
       </header>
     </div>
   );
 }
+
 
 export default App;
---
+++ frontend/src/Login.js
@@ -0,0 +1,34 @@
+import './index.css';
+import aviaobackground from './aviaobackground.jpg'
+import aviaologo from './aviaologo.png'
+
+function Login(){
+    return(
+        <div className = "Login">
+            <div id ="panel-left">
+                <img src={aviaologo} width='50px'/>
+                <h4 className="text">  Slim Aircraft Manual Composer</h4>
+                <hr/>
+                <img id="foto-perf" src={aviaologo} width= '150px'/>
+                <h1 className="text">Slim</h1><br/>
+
+                <div id="login-inputs">
+                    <h6 className="text">E-mail</h6><br/>
+                    <input type='text'/><br/><br/>
+
+                    <h6 className="text">Senha</h6><br/>
+                    <input type='password'/><br/><br/>
+
+                    <input type='checkbox'/>
+                    <h6 className="text link">Continuar conectado</h6><br/><br/>
+
+                    <a href="#"><h6 className="text link">Esqueceu a senha?</h6></a>
+                    <button onClick="">Login</button>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Login;
